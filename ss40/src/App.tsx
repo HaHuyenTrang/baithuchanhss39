286e75645bfb1fbd63577661cb9ede222bb8ea74
HaHuyenTrang <huyentrang15625@gmail.com>
2024-05-26 22:33:39 +0700

    update

---
+++ ss40/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Manage_student from './components/Manage_student'
+
+export default function App() {
+  return (
+    <div>App
+
+    /<Manage_student></Manage_student>''
+    </div>
+  )
+}
